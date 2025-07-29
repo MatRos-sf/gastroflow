b2796f351b33e4536e26a38c5425a06313f5d111
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-07-29 20:00:56 +0200

    register admin site

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,8 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from .models import Addition, Item
 
 # Register your models here.
+
+admin.site.register(Item)
+admin.site.register(Addition)
