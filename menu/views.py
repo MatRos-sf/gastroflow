87ccb664f71cbed336e4589670bf6f7dcee3dfe4
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-08-02 03:33:01 +0200

    .

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,13 @@
 # from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Item
+from .serializers import ItemSerializer
+
+
+class ItemListView(APIView):
+    def get(self, request):
+        items = Item.objects.exclude(menu__isnull=True).exclude(menu="")
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
