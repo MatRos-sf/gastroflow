acc394fd454184579b66b4a019c338b69a7052cb
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-08-07 20:43:04 +0200

    feat(order): create helper properties

--- order/admin.py
+++ order/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Order, OrderItem, OrderItemAddition
+from .models import Bill, Order, OrderItem, OrderItemAddition
 
 # Register your models here.
+admin.site.register(Bill)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(OrderItemAddition)
