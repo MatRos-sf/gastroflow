ea752095162912ec680888d7006bc9ec102f92a3
mateusz.rosenkranz@apator.com <mateusz.rosenkranz@apator.com>
2025-08-09 08:56:17 +0200

    feat(kitchen) add kitchen history

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import kitchen_orders_view
+from .views import kitchen_orders_view, HistoryOrderKitchen
 
 urlpatterns = [
     path("orders/", kitchen_orders_view, name="kitchen-orders"),
+    path("history/", HistoryOrderKitchen.as_view(), name="orders-history"),
+
 ]
