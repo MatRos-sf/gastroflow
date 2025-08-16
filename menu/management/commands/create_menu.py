15634d0973aee197f91ce00093c5ac583f698b87
mateusz.rosenkranz@apator.com <mateusz.rosenkranz@apator.com>
2025-08-16 09:41:02 +0200

    fix(item): add missing additions

--- menu/management/commands/create_menu.py
+++ menu/management/commands/create_menu.py
@@ -16,6 +16,7 @@ default_menu = [
             {"name": "Szynka", "id_checkout": 16, "price": Decimal("2.00")},
             {"name": "Boczek", "id_checkout": 17, "price": Decimal("3.00")},
             {"name": "Pomidor", "id_checkout": 18, "price": Decimal("1.00")},
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
         ],
         "id_checkout": 1,
         "price": Decimal("25.00"),
@@ -25,7 +26,9 @@ default_menu = [
         "preparation_location": Location.KITCHEN,
         "name": "Jajka W Koszulce",
         "description": "jogurt grecki / czosnek / nasz autorski olej chilli/ zaatar / koperek / pieczywo",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 2,
         "price": Decimal("29.00"),
     },
@@ -34,7 +37,9 @@ default_menu = [
         "preparation_location": Location.KITCHEN,
         "name": "Autorski Omlet Witka",
         "description": "omlet z 3 jaj / wędzony pstrąg z lokalnej wędzarni / sos holenderski / ser Bursztyn / szczypiorek / mix sałat ze słonecznikiem",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 3,
         "price": Decimal("34.00"),
     },
@@ -43,7 +48,9 @@ default_menu = [
         "preparation_location": Location.KITCHEN,
         "name": "Bajgel Klasyczny: Z łososiem",
         "description": "serek śmietankowy / wędzony łosoś z lokalnej wędzarni / rukola / ogórek / mix sałat ze słonecznikiem",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 5,
         "price": Decimal("36.00"),
     },
@@ -52,7 +59,9 @@ default_menu = [
         "preparation_location": Location.KITCHEN,
         "name": "Bajgel Klasyczny: Z szarpanym kurczakiem",
         "description": "pasta ze szarpanym kurczakiem / pomidor / rukola / ogórek / mix sałat ze słonecznikiem",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 6,
         "price": Decimal("36.00"),
     },
@@ -61,7 +70,9 @@ default_menu = [
         "preparation_location": Location.KITCHEN,
         "name": "Kiełbasa Podsmarzana",
         "description": "jajka sadzone / pieczywo / sosy / mix sałat ze słonecznikiem",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 7,
         "price": Decimal("29.00"),
     },
@@ -70,7 +81,9 @@ default_menu = [
         "preparation_location": Location.KITCHEN,
         "name": "Maślane Tosty Francuskie",
         "description": "jogurt grecki / owoce sezonowe / syrop klonowy / mięta",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 8,
         "price": Decimal("28.00"),
     },
@@ -79,7 +92,9 @@ default_menu = [
         "preparation_location": Location.KITCHEN,
         "name": "Ruchanki Kaszubskie",
         "description": "drożdżowe placki z jabłkiem / kwaśna śmietana / domowa konfitura z borówek / maliny / mięta",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 9,
         "price": Decimal("28.00"),
     },
@@ -89,7 +104,9 @@ default_menu = [
         "preparation_location": Location.KITCHEN,
         "name": "Śniadanie małego pirata",
         "description": "jajecznica / naturalne parówki ośmiorniczki / pomidory / ogórki / bułka",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 10,
         "price": Decimal("22.00"),
     },
@@ -98,7 +115,9 @@ default_menu = [
         "preparation_location": Location.KITCHEN,
         "name": "Śniadaniowy Żagiel",
         "description": "chleb / masło / szynka / ser żółty / ogórek / pomidor",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 11,
         "price": Decimal("18.00"),
     },
@@ -107,7 +126,9 @@ default_menu = [
         "preparation_location": Location.KITCHEN,
         "name": "Skarb Słodkiego Pirata",
         "description": "pancakes / nutella / truskawki / borówki",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 12,
         "price": Decimal("21.00"),
     },
@@ -116,7 +137,9 @@ default_menu = [
         "preparation_location": Location.KITCHEN,
         "name": "Słodki Krab",
         "description": "croissant / domowy dżem truskawkowy / truskawki / banan / borówki",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 13,
         "price": Decimal("22.00"),
     },
@@ -127,7 +150,9 @@ default_menu = [
         "sub_menu": SubMenuType.COFFEE,
         "name": "Espresso",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 20,
         "price": Decimal("9.00"),
     },
@@ -137,7 +162,9 @@ default_menu = [
         "sub_menu": SubMenuType.COFFEE,
         "name": "Americano",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 22,
         "price": Decimal("12.00"),
     },
@@ -147,7 +174,9 @@ default_menu = [
         "sub_menu": SubMenuType.COFFEE,
         "name": "Cappucino",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 21,
         "price": Decimal("14.00"),
     },
@@ -157,7 +186,9 @@ default_menu = [
         "sub_menu": SubMenuType.COFFEE,
         "name": "Latte Macchiato",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 23,
         "price": Decimal("16.00"),
     },
@@ -167,7 +198,9 @@ default_menu = [
         "sub_menu": SubMenuType.COFFEE,
         "name": "Fusiara Kaszubska",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 24,
         "price": Decimal("9.00"),
     },
@@ -177,7 +210,9 @@ default_menu = [
         "sub_menu": SubMenuType.COFFEE,
         "name": "Kawa Mrożona",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 25,
         "price": Decimal("19.00"),
     },
@@ -187,7 +222,9 @@ default_menu = [
         "sub_menu": SubMenuType.TEA,
         "name": "Czarna",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 26,
         "price": Decimal("10.00"),
     },
@@ -197,7 +234,9 @@ default_menu = [
         "sub_menu": SubMenuType.TEA,
         "name": "Earl Grey",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 26,
         "price": Decimal("10.00"),
     },
@@ -207,7 +246,9 @@ default_menu = [
         "sub_menu": SubMenuType.TEA,
         "name": "Zielona",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 26,
         "price": Decimal("10.00"),
     },
@@ -217,7 +258,9 @@ default_menu = [
         "sub_menu": SubMenuType.TEA,
         "name": "Jaśminowa",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 26,
         "price": Decimal("10.00"),
     },
@@ -227,7 +270,9 @@ default_menu = [
         "sub_menu": SubMenuType.TEA,
         "name": "Owocowa",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 26,
         "price": Decimal("10.00"),
     },
@@ -237,7 +282,9 @@ default_menu = [
         "sub_menu": SubMenuType.TEA,
         "name": "Jabłkowa",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 26,
         "price": Decimal("10.00"),
     },
@@ -247,7 +294,9 @@ default_menu = [
         "sub_menu": SubMenuType.TEA,
         "name": "Miętowa",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 26,
         "price": Decimal("10.00"),
     },
@@ -257,7 +306,9 @@ default_menu = [
         "sub_menu": SubMenuType.MATCHA,
         "name": "Matcha latte (na ciepło)",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 27,
         "price": Decimal("18.00"),
     },
@@ -267,7 +318,9 @@ default_menu = [
         "sub_menu": SubMenuType.MATCHA,
         "name": "Matcha latte truskawkowa (na zimno)",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 28,
         "price": Decimal("24.00"),
     },
@@ -277,7 +330,9 @@ default_menu = [
         "sub_menu": SubMenuType.MATCHA,
         "name": "Matcha latte pistacja-wanilia (na zimno)",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 29,
         "price": Decimal("26.00"),
     },
@@ -287,7 +342,9 @@ default_menu = [
         "sub_menu": SubMenuType.MATCHA,
         "name": "Matcha latte mango (na zimno)",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 30,
         "price": Decimal("25.00"),
     },
@@ -393,7 +450,9 @@ default_menu = [
         "preparation_location": Location.BAR,
         "name": "Lemoniada Cytrynowa",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 38,
         "price": Decimal("13.00"),
     },
@@ -402,7 +461,9 @@ default_menu = [
         "preparation_location": Location.BAR,
         "name": "Lemoniada Truskawkowa",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 39,
         "price": Decimal("14.00"),
     },
@@ -411,7 +472,9 @@ default_menu = [
         "preparation_location": Location.BAR,
         "name": "Lemoniada Mango",
         "description": "",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 40,
         "price": Decimal("15.00"),
     },
@@ -431,7 +494,9 @@ default_menu = [
         "sub_menu": SubMenuType.COCKTAIL,
         "name": "Kremowa Truskawka",
         "description": "truskawki / jogurt / śmietana",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 42,
         "price": Decimal("18.00"),
     },
@@ -441,7 +506,9 @@ default_menu = [
         "sub_menu": SubMenuType.COCKTAIL,
         "name": "Zielona Energia",
         "description": "szpinak / banan / mango / cytryna / pomarańcz ",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 43,
         "price": Decimal("20.00"),
     },
@@ -451,7 +518,9 @@ default_menu = [
         "sub_menu": SubMenuType.COCKTAIL,
         "name": "Dodający Energii",
         "description": "banan / płatki owsiane / masło orzechowe / mleko / gorzka czekolada",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 44,
         "price": Decimal("21.00"),
     },
@@ -461,7 +530,9 @@ default_menu = [
         "sub_menu": SubMenuType.SOFT_DRINK,
         "name": "Bezowe Pitku",
         "description": "limonka / syrop z kwiatów bzu / prosseco 0% / mięta",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 45,
         "price": Decimal("24.00"),
     },
@@ -471,7 +542,9 @@ default_menu = [
         "sub_menu": SubMenuType.SOFT_DRINK,
         "name": "Miętowe Pitku",
         "description": "limonka / mięta / rum 0%",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 46,
         "price": Decimal("24.00"),
     },
@@ -481,7 +554,9 @@ default_menu = [
         "sub_menu": SubMenuType.SOFT_DRINK,
         "name": "Pomarańczowe Pitku",
         "description": "pomarańcza / aperitivo 0% / prosseco 0%",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 47,
         "price": Decimal("24.00"),
     },
@@ -491,18 +566,11 @@ default_menu = [
         "sub_menu": SubMenuType.SOFT_DRINK,
         "name": "Lawendowe Pitku",
         "description": "syrop lawendowy / gin 0% / białko",
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 48,
         "price": Decimal("24.00"),
-    },
-    # MenuType.OTHER
-    {
-        "menu": MenuType.OTHER,
-        "name": "Opakowanie",
-        "description": "",
-        "additions": [],
-        "id_checkout": 59,
-        "price": Decimal("2.00"),
     },
     # MenuType.DESSERT
     {
@@ -511,7 +579,9 @@ default_menu = [
         "description": "",
         "sub_menu": SubMenuType.CAKE,
         "preparation_location": Location.BAR,
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 19,
         "price": Decimal("22.00"),
     },
@@ -521,7 +591,9 @@ default_menu = [
         "description": "",
         "sub_menu": SubMenuType.CAKE,
         "preparation_location": Location.BAR,
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 19,
         "price": Decimal("24.00"),
     },
@@ -531,7 +603,9 @@ default_menu = [
         "description": "",
         "sub_menu": SubMenuType.CAKE,
         "preparation_location": Location.BAR,
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 49,
         "price": Decimal("25.00"),
     },
@@ -541,7 +615,9 @@ default_menu = [
         "description": "",
         "sub_menu": SubMenuType.CAKE,
         "preparation_location": Location.BAR,
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 50,
         "price": Decimal("16.00"),
     },
@@ -550,7 +626,9 @@ default_menu = [
         "name": "Owoce z Białą Czekoladą",
         "description": "",
         "preparation_location": Location.BAR,
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 60,
         "price": Decimal("16.00"),
     },
@@ -559,7 +637,9 @@ default_menu = [
         "name": "Cake Pop",
         "description": "",
         "preparation_location": Location.BAR,
-        "additions": [],
+        "additions": [
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
+        ],
         "id_checkout": 58,
         "price": Decimal("12.00"),
     },
@@ -574,10 +654,20 @@ default_menu = [
             {"name": "Owoce", "id_checkout": 55, "price": Decimal("4.00")},
             {"name": "Posypka", "id_checkout": 56, "price": Decimal("1.00")},
             {"name": "Nutella", "id_checkout": 57, "price": Decimal("4.00")},
+            {"name": "Opakowanie", "id_checkout": 51, "price": Decimal("2.00")},
         ],
         "id_checkout": 53,
         "price": Decimal("12.00"),
     },
+]
+
+addition_collection = [
+    {"name": "Mleko roślinne", "id_checkout": 59, "price": Decimal("2.00")},
+    {"name": "Jajko", "id_checkout": 61, "price": Decimal("4.00")},
+    {"name": "Koszyk Pieczywa", "id_checkout": 62, "price": Decimal("10.00")},
+    {"name": "Bułka", "id_checkout": 63, "price": Decimal("3.00")},
+    {"name": "Dodatek", "id_checkout": 64, "price": Decimal("4.00")},
+    {"name": "Opakowanie", "id_checkout": 0, "price": Decimal("4.00")},
 ]
 
 
@@ -591,7 +681,17 @@ class Command(BaseCommand):
                 obj, _ = Addition.objects.get_or_create(**addition)
                 if _:
                     print("\t\tSaved addition:", obj.name)
+                else:
+                    print(f"\t\tAdd existing additions ({obj})")
                 additions.append(obj)
             obj, _ = Item.objects.get_or_create(**item)
             obj.additions.set(additions)
             print("Saved item:", obj.name)
+
+        # add additions
+        for addition in addition_collection:
+            obj, _ = Addition.objects.get_or_create(**addition)
+            if _:
+                print("\t\tSaved addition:", obj.name)
+            else:
+                print(f"\t\tSkipp addition: ({obj})")
