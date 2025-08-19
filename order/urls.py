8e0c9112426d3865f83568de665ca58cde2fc368
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-08-19 18:56:03 +0200

    feat(Bill): add update discount view

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import daily_report
+from .views import daily_report, update_discount
 
 urlpatterns = [
     path("", daily_report, name="daily-report"),
+    path("update/discount/<int:pk>", update_discount, name="update-discount"),
 ]
