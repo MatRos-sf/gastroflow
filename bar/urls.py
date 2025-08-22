d26d74cac406aedef7042ff97cf8a291dec64cdf
mateusz.rosenkranz@apator.com <mateusz.rosenkranz@apator.com>
2025-08-22 16:46:17 +0200

    fix(UI) new layout

--- bar/urls.py
+++ bar/urls.py
@@ -4,5 +4,5 @@ from .views import BarOrderKitchen, bar_orders
 
 urlpatterns = [
     path("orders/", bar_orders, name="bar-orders"),
-    path("history/", BarOrderKitchen.as_view(), name="orders-history"),
+    path("history/", BarOrderKitchen.as_view(), name="orders-bar-history"),
 ]
