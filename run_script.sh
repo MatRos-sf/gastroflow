3653d8a89eed128833c6fb42f21252d81e9754a1
mateusz.rosenkranz@apator.com <mateusz.rosenkranz@apator.com>
2025-08-22 23:47:19 +0200

    feat(daphne) add daphne

--- run_script.sh
+++ run_script.sh
@@ -3,10 +3,14 @@ echo "1"
 set -euo pipefail
 echo "2"
 cd "$HOME/Desktop/Frisztek/gastroflow"
-sleep 10 
+sleep 10
+
+
 pkill -f "manage.py runserver" || true
 git pull
-. venv/bin/activate 
+. venv/bin/activate
 pip install -r requirements.txt
 
-python manage.py runserver 0.0.0.0:8000
+sudo docker start redis
+
+daphne -p 8000 gastroflow.asgi:application
