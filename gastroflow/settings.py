6ace29a0b650318015cedad333bc9d447e499dff
MatRos-sf <mateuszrosenkranz@gmail.com>
2026-03-03 22:05:55 +0100

    feat: change global base templates

--- gastroflow/settings.py
+++ gastroflow/settings.py
@@ -83,7 +83,7 @@ ROOT_URLCONF = "gastroflow.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
