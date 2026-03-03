bad6121a9292bf4c46cca3c94654484321caa550
Mateusz <59665130+MatRos-sf@users.noreply.github.com>
2026-03-03 22:15:13 +0100

    Update global templates and add new translations (#58)
    * feat: change global base templates
    
    * feat: verbose_name, help_text with _()
    
    * feat: add new translation
    
    * feat: add new create-worker template

--- worker/models.py
+++ worker/models.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -7,24 +9,50 @@ from tools.models.validators import validate_digits
 class Position(models.TextChoices):
     WAITER = "waiter", _("Waiter")
     CHEF = "chef", _("Chef")
-    ASSISTANT = "assistant", _("Asystent")
+    ASSISTANT = "assistant", _("Assistant")
     BARISTA = "barista", _("Barista")
 
 
 class Worker(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30, verbose_name=_("first name"))
+    last_name = models.CharField(max_length=30, verbose_name=_("last name"))
 
-    salary = models.DecimalField(max_digits=7, decimal_places=2, default=0.00)
+    salary = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        default=0.00,
+        help_text=_("per hour"),
+        verbose_name=_("salary"),
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
     position = models.CharField(
-        max_length=20, choices=Position.choices, default=Position.WAITER
+        max_length=20,
+        choices=Position.choices,
+        default=Position.WAITER,
+        verbose_name=_("position"),
     )
     pin = models.CharField(
-        max_length=4, blank=True, null=True, validators=[validate_digits]
+        max_length=4,
+        blank=True,
+        null=True,
+        validators=[validate_digits],
+        verbose_name=_("pin"),
     )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class WorkTime(models.Model):
+    worker = models.ForeignKey(Worker, on_delete=models.SET_NULL, null=True)
+    start_time = models.DateTimeField()
+    finish_time = models.DateTimeField()
+
+    @property
+    def duration(self) -> timedelta:
+        return self.finish_time - self.start_time
+
+    def __str__(self):
+        return f"Worker: {self.duration}"
