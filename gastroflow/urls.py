fa881877599aa50d01f64c33d80fca53dfb66302
MatRos-sf <mateuszrosenkranz@gmail.com>
2026-03-07 20:29:14 +0100

    feat: add auth

--- gastroflow/urls.py
+++ gastroflow/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("menu/", include("menu.urls")),
