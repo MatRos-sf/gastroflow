82ee93bfc09a0f4f120f8f4e849a06def3cce1d3
Mateusz <59665130+MatRos-sf@users.noreply.github.com>
2026-03-12 21:09:44 +0100

    Feat/menu daily stock (#68)
    * delete ItemMenuTypeFilter
    * remove AvailableListView, toggle_availability, delivery_items
    * add templates for creating views
    * add translation

--- menu/urls.py
+++ menu/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 
 from .views import (
-    AvailableListView,
     ItemCreateView,
+    ItemDailyStockListView,
     ItemDetailView,
     ItemListView,
+    ItemReplenishView,
     ItemUpdateView,
     addition_quick_create,
     category_quick_create,
-    delivery_items,
+    set_daily_stock,
     subcategory_quick_create,
-    toggle_availability,
+    supply_all_items,
+    supply_item,
 )
 
 app_name = "gf-menu"
@@ -20,9 +22,13 @@ urlpatterns = [
     path("item/create/", ItemCreateView.as_view(), name="item-create"),
     path("item/update/<int:pk>/", ItemUpdateView.as_view(), name="item-update"),
     path("item/<int:pk>/", ItemDetailView.as_view(), name="item-detail"),
-    path("changer/", AvailableListView.as_view(), name="available"),
-    path("toggle/<int:pk>/", toggle_availability, name="toggle-availability"),
-    path("delivery-product/", delivery_items, name="delivery-product"),
+    path(
+        "item/daily-stock/", ItemDailyStockListView.as_view(), name="item-daily-stock"
+    ),
+    path("item/replenish/", ItemReplenishView.as_view(), name="item-replenish"),
+    path("item/<int:pk>/set-stock/", set_daily_stock, name="item-set-stock"),
+    path("item/<int:pk>/supply/", supply_item, name="item-supply"),
+    path("item/supply-all/", supply_all_items, name="item-supply-all"),
     path("addition/quick-create/", addition_quick_create, name="addition-quick-create"),
     path("category/quick-create/", category_quick_create, name="category-quick-create"),
     path(
