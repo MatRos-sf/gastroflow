503f7cf5187c5d6a64eb27faa322cafc3d593272
MatRos-sf <mateuszrosenkranz@gmail.com>
2026-03-24 22:52:51 +0100

    feat: add login  required for consumer views

--- consumers/views.py
+++ consumers/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required
 def bar_orders_view(request):
     return render(request, "consumers/bar/orders.html")
 
 
+@login_required
 def kitchen_orders_view(request):
     return render(request, "consumers/kitchen/orders.html")
