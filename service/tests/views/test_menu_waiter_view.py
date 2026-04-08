0074f6f6abc29c9bd7d849e491e98faf9f0a0ab7
MatRos-sf <mateuszrosenkranz@gmail.com>
2026-04-08 21:12:03 +0200

    test(service): refactor MenuWaiterView tests to match current implementation

--- service/tests/views/test_menu_waiter_view.py
+++ service/tests/views/test_menu_waiter_view.py
@@ -1,76 +1,86 @@
-from django.contrib.messages import get_messages
-from django.contrib.messages.storage.fallback import FallbackStorage
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 from model_bakery import baker
 
-from menu.models import Item, MenuType, SubMenuType
-from service.views import MenuWaiterView
+from menu.models import Category, Item, SubCategory
+from service.views.order_flow.menu import MenuWaiterView
 
 
 class MenuWaiterViewTest(TestCase):
-    ENDPOINT = reverse("service:menu-waiter")
-    VIEW_CLASS = MenuWaiterView
+    ENDPOINT = reverse("service:order-select-items")
 
     @classmethod
     def setUpTestData(cls):
-        baker.make("menu.Item", _quantity=5, menu=MenuType.MAIN)
-        baker.make(
-            "menu.Item", _quantity=3, menu=MenuType.DRINK, sub_menu=SubMenuType.COFFEE
-        )
-        baker.make(
-            "menu.Item", _quantity=2, menu=MenuType.DRINK, sub_menu=SubMenuType.TEA
-        )
+        cls.category_main = baker.make(Category)
+        cls.category_drinks = baker.make(Category)
+        cls.sub_coffee = baker.make(SubCategory)
+        cls.sub_tea = baker.make(SubCategory)
 
-        cls.ITEM_MAIN = Item.objects.filter(menu=MenuType.MAIN)
-        cls.ITEM_DRINK = Item.objects.filter(menu=MenuType.DRINK)
+        baker.make(Item, _quantity=5, category=cls.category_main, is_delete=False)
+        baker.make(
+            Item,
+            _quantity=3,
+            category=cls.category_drinks,
+            sub_menu=cls.sub_coffee,
+            is_delete=False,
+        )
+        baker.make(
+            Item,
+            _quantity=2,
+            category=cls.category_drinks,
+            sub_menu=cls.sub_tea,
+            is_delete=False,
+        )
 
     def setUp(self):
         self.factory = RequestFactory()
 
-    def test_check_db(self):
-        """Simple test verify that all object in table are created"""
-        self.assertEqual(Item.objects.count(), 10)
+    def _make_view(self, params=None):
+        request = self.factory.get(self.ENDPOINT, params or {})
+        view = MenuWaiterView()
+        view.setup(request)
+        return view
 
-    def test_get(self):
+    def test_get_returns_200(self):
         response = self.client.get(self.ENDPOINT)
         self.assertEqual(response.status_code, 200)
-        # self.assertTemplateUsed(response, self.VIEW_CLASS.template_name)
+        self.assertTemplateUsed(response, MenuWaiterView.template_name)
 
-    def test_verify_context_manager(self):
-        request = self.factory.get(self.ENDPOINT, {"category": MenuType.DRINK.value})
+    def test_queryset_filters_items_by_category(self):
+        view = self._make_view({"category": self.category_drinks.pk})
+        self.assertEqual(view.get_queryset().count(), 5)  # 3 coffee + 2 tea
 
-        view = self.VIEW_CLASS()
-        view.setup(request)
+    def test_queryset_returns_empty_when_no_categories_exist(self):
+        Category.objects.all().delete()
+        view = self._make_view()
+        self.assertEqual(view.get_queryset().count(), 0)
+
+    def test_context_groups_items_by_sub_menu(self):
+        view = self._make_view({"category": self.category_drinks.pk})
         ctx = view.get_context_data(object_list=view.get_queryset())
 
         sub_menu_groups = ctx["sub_menu_groups"]
-
         self.assertEqual(len(sub_menu_groups), 2)
-        self.assertEqual(sub_menu_groups[SubMenuType.COFFEE.value].count(), 3)
-        self.assertEqual(sub_menu_groups[SubMenuType.TEA.value].count(), 2)
+        self.assertEqual(sub_menu_groups[self.sub_coffee].count(), 3)
+        self.assertEqual(sub_menu_groups[self.sub_tea].count(), 2)
 
+    def test_context_items_no_sub_excludes_items_with_sub_menu(self):
+        view = self._make_view({"category": self.category_drinks.pk})
+        ctx = view.get_context_data(object_list=view.get_queryset())
         self.assertEqual(ctx["items_no_sub"].count(), 0)
 
-    def test_mock_should_prepare_context_for_main_category_when_category_is_invalid(
-        self,
-    ):
-        wrong_category = "test"
-        request = self.factory.get(self.ENDPOINT, {"category": wrong_category})
-
-        # CHEAT Django tha thought that MessageMiddleware is already turn on
-        request.session = {}
-        messages = FallbackStorage(request)
-        setattr(request, "_messages", messages)
-        ######
-
-        view = self.VIEW_CLASS()
-        view.setup(request)
+    def test_context_items_no_sub_includes_items_without_sub_menu(self):
+        view = self._make_view({"category": self.category_main.pk})
         ctx = view.get_context_data(object_list=view.get_queryset())
+        self.assertEqual(ctx["items_no_sub"].count(), 5)
+        self.assertEqual(len(ctx["sub_menu_groups"]), 0)
 
-        self.assertEqual(ctx["selected_category"], wrong_category)
-        self.assertEqual(ctx["object_list"].count(), self.ITEM_MAIN.count())
+    def test_context_selected_category_matches_requested_category(self):
+        view = self._make_view({"category": self.category_drinks.pk})
+        ctx = view.get_context_data(object_list=view.get_queryset())
+        self.assertEqual(ctx["selected_category"], self.category_drinks)
 
-        stored_message = list(get_messages(request))
-        self.assertEqual(len(stored_message), 1)
-        self.assertEqual(stored_message[0].message, "Invalid category: test")
+    def test_context_defaults_to_first_category_on_invalid_pk(self):
+        view = self._make_view({"category": "not-a-number"})
+        ctx = view.get_context_data(object_list=view.get_queryset())
+        self.assertEqual(ctx["selected_category"], Category.objects.first())
