69742984193710a635745e381e42ef480fbb48e3
MatRos-sf <mateuszrosenkranz@gmail.com>
2026-04-10 22:02:13 +0200

    feat: add date filtering and new report sections to ReportView

--- order/_views/report_views.py
+++ order/_views/report_views.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from typing import Any
 
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -6,24 +5,16 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.views.generic import View
 
+from order.forms import DateForm
 from order.report import generate_summary_report
-from tools.report_calculator import CALCULATOR_BASIC
-
-
-class ReportDispatchView(LoginRequiredMixin, View):
-    def get(self, request, *args, **kwargs):
-        if request.user.is_superuser:
-            return ReportView.as_view()(request, *args, **kwargs)
-        return ReportBasicView.as_view()(request, *args, **kwargs)
+from tools.report_calculator import CALCULATOR_BASIC, CALCULATOR_COLLECTION
 
 
 class ReportBasicView(View):
     template_name = "order/report/report_base.html"
     calculator_collection = CALCULATOR_BASIC
 
-    def build_context(
-        self, from_date: datetime | None, to_date: datetime | None
-    ) -> dict[str, Any]:
+    def build_context(self, from_date, to_date) -> dict[str, Any]:
         return {
             "date_from": from_date,
             "date_to": to_date,
@@ -38,4 +29,30 @@ class ReportBasicView(View):
 
 
 class ReportView(ReportBasicView):
-    template_name = "order/report/report_base.html"
+    template_name = "order/report/report.html"
+    calculator_collection = CALCULATOR_COLLECTION
+
+    def get(self, request, *args, **kwargs):
+        today = timezone.now().date()
+        form = DateForm(request.GET or None)
+
+        if request.GET and form.is_valid():
+            from_date = form.cleaned_data.get("from_date")
+            to_date = form.cleaned_data.get("to_date")
+            if to_date and not from_date:
+                from_date = today
+        else:
+            from_date = today
+            to_date = None
+            form = DateForm(initial={"from_date": from_date})
+
+        context = self.build_context(from_date, to_date)
+        context["form"] = form
+        return render(request, self.template_name, context)
+
+
+class ReportDispatchView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_superuser:
+            return ReportView.as_view()(request, *args, **kwargs)
+        return ReportBasicView.as_view()(request, *args, **kwargs)
