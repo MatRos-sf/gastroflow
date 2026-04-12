aa4faa2c6434cdf102ec8a7e18bc41426c1854bd
MatRos-sf <mateuszrosenkranz@gmail.com>
2026-04-12 16:08:38 +0200

    refactor: rename package _views into view

--- order/urls.py
+++ order/urls.py
@@ -1,35 +1,35 @@
 from django.urls import path
 
-import order._views.bill_views as _views
-import order._views.order_views as _views_order
-import order._views.report_views as _views_report
+import order.views.bill_views as bill_view
+import order.views.order_views as order_view
+import order.views.report_views as report_view
 
 # TODO: name app "order"
 
 url_report_pattern = [
-    path("report/", _views_report.ReportDispatchView.as_view(), name="report"),
-    path(
-        "report/generate/", _views_report.generate_report_view, name="report-generate"
-    ),
+    path("report/", report_view.ReportDispatchView.as_view(), name="report"),
+    path("report/generate/", report_view.generate_report_view, name="report-generate"),
 ]
 
 urlpatterns = [
-    path("bill/", _views.BillListView.as_view(), name="list-bill"),
-    path("bill/<int:pk>/", _views.BillDetailView.as_view(), name="detail-bill"),
-    path("bill/<int:pk>/delete/", _views.BillDeleteView.as_view(), name="delete-bill"),
-    path("bill/<int:pk>/edit/", _views.edit_bill, name="edit-bill"),
-    path("bill/<int:pk>/close/", _views.close_bill, name="close-bill"),
-    path("bill/<int:pk>/discount/", _views.add_discount, name="add-discount"),
+    path("bill/", bill_view.BillListView.as_view(), name="list-bill"),
+    path("bill/<int:pk>/", bill_view.BillDetailView.as_view(), name="detail-bill"),
+    path(
+        "bill/<int:pk>/delete/", bill_view.BillDeleteView.as_view(), name="delete-bill"
+    ),
+    path("bill/<int:pk>/edit/", bill_view.edit_bill, name="edit-bill"),
+    path("bill/<int:pk>/close/", bill_view.close_bill, name="close-bill"),
+    path("bill/<int:pk>/discount/", bill_view.add_discount, name="add-discount"),
     path(
         "bill/<int:pk>/close-with-release/",
-        _views.close_bill_with_release,
+        bill_view.close_bill_with_release,
         name="bill-close-release",
     ),
-    path("<int:pk>/delete/", _views_order.delete_order, name="delete-order"),
+    path("<int:pk>/delete/", order_view.delete_order, name="delete-order"),
     path(
         "item/<int:pk_item>/delete/",
-        _views_order.delete_ordered_item,
+        order_view.delete_ordered_item,
         name="delete-order-item",
     ),
-    path("ready/", _views_order.ReadyOrderListView.as_view(), name="ready-order-list"),
+    path("ready/", order_view.ReadyOrderListView.as_view(), name="ready-order-list"),
 ] + url_report_pattern
