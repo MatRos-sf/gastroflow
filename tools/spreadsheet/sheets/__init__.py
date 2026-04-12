5d1c195c1e663fc22c9965dae0ad022bcc50d168
MatRos-sf <mateuszrosenkranz@gmail.com>
2026-04-12 16:26:55 +0200

    refactor: remove obsolate views

--- tools/spreadsheet/sheets/__init__.py
+++ tools/spreadsheet/sheets/__init__.py
@@ -1,12 +1,6 @@
 from .order_detail import OrderAdditionSheet, OrderItemSheet
-from .order_list import TableWriterSheet
-from .stats import StatsSheet
-from .worker_stats import WorkerStatsSheet
 
 __all__ = [
     "OrderItemSheet",
     "OrderAdditionSheet",
-    "TableWriterSheet",
-    "WorkerStatsSheet",
-    "StatsSheet",
 ]
